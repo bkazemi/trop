7ee577c0ee15826502ea9898ff3828e061d6b763
bk <bkazemi@users.noreply.github.com>
2015-10-23 03:26:51 -0700

    v1.2.2
    Fixed a bug in check_tracker_errors().

--- trop.sh
+++ trop.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 TROP_VERSION=\
-'trop 1.2.1
+'trop 1.2.2
 last checked against: transmission-remote 2.84 (14307)'
 
 usage ()
@@ -538,12 +538,14 @@ check_tracker_errors ()
 {
 	## $1 - silence warning
 
-	set +e
 	trop_private
 	trop_torrent l | awk '
+		BEGIN { ret = 0 }
 		$1 ~ /\*/ {
-			exit 1
+			if (!ret)
+				ret=1
 		}
+		END { exit ret }
 	' && return 1 || { [ -z "$1" ] && _e 26 ;}
 
 	return 0
@@ -599,7 +601,7 @@ PROG_NAME=${0##*/}
 LC_ALL=POSIX
 toppid=$$
 silent=0
-trap 'set -e ; exit 1' 6
+trap 'exit 1' 6
 hash transmission-remote 2>/dev/null || die 5
 
 # check if file used to call the script is a link or the script file itself

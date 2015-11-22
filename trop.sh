bkazemi/trop

commit 24f2145d8dc1775e978bfc1ecda45c73758fb13c

trop_torrent option checking
trop_torrent will now interally check to see if the user entered a
valid command. I initially did not want to go this route but the
cost is cheap, tr-remote options are rarely added, and it helps
with trop_torrent_done().

Other changes:
- trop.sh: trop_torrent_done will now take any valid trop_torrent
           options.
- man page + README polishing for v1.6

trop.sh
@@ -308,8 +308,46 @@ trop_torrent ()
 		return 0
 	fi
 
+	local thirdopt=0
+	case $2 in
+	d|downlimit) thirdopt=1 ;;
+	D|no-downlimit);;
+	f|files);;
+	i|info);;
+	ip|info-peers);;
+	ic|info-pieces);;
+	it|info-trackers);;
+	Bh|bandwith-high);;
+	Bn|bandwith-normal);;
+	Bl|bandwith-low);;
+	pr|peers) thirdopt=1 ;;
+	r|remove);;
+	R|remove-and-delete);;
+	reannounce);;
+	move) thirdopt=1 ;;
+	find) thirdopt=1 ;;
+	sr|seedratio) thirdopt=1 ;;
+	SR|no-seedratio);;
+	srd|seedratio-default);;
+	td|tracker-add) thirdopt=1 ;;
+	tr|tracker-remove) thirdopt=1 ;;
+	s|start);;
+	S|stop);;
+	hl|honor-session);;
+	HL|no-honor-session);;
+	u|uplimit) thirdopt=1 ;;
+	U|no-uplimit);;
+	no-utp);;
+	v|verify);;
+	pi|peer-info);;
+	*) die $ERR_TT_UNKNOWN_OPT ;;
+	esac
+
 	[ ${#2} -gt 2 ] && opt="--${2}" || opt="-${2}"
-	transmission-remote $(hpc) -n "$AUTH" -t $1 $opt || die $ERR_TR_FAIL
+	{ [ $thirdopt -eq 1 ]                                 && \
+	transmission-remote $(hpc) -n "$AUTH" -t $1 $opt "$3" || \
+	transmission-remote $(hpc) -n "$AUTH" -t $1 $opt         \
+	;} || die $ERR_TR_FAIL
 
 	return 0
 }
@@ -329,18 +367,7 @@ trop_torrent_done ()
 		cat ${srcdir}/.cache/tdscript | while read tid; do
 			[ "${tid%% *}" = "$1" ] && die $ERR_TTD_SCHG
 		done
-		case $2 in
-		rm)
-			[ "$3" = 'hard' ] && rmcmd='remove-and-delete' || rmcmd='r'
-			echo "$1" ${rmcmd} >> ${srcdir}/.cache/tdscript
-			;;
-		stop)
-			echo "$1" 'S' >> ${srcdir}/.cache/tdscript
-			;;
-		*)
-			die $ERR_TTD_UNKNOWN_ACT
-			;;
-		esac
+		echo "$@" >> ${srcdir}/.cache/tdscript
 		return 0
 	fi
 	local nr=0 tid
@@ -527,6 +554,9 @@ ERR_TMF_UNKNOWN_FTYPE=3
 ERR_PC_STDIN_EMPTY=4
 ERR_PC_NO_INPUT=5
 
+# trop_torrent
+ERR_TT_UNKNOWN_OPT=12
+
 # trop_tracker_total
 ERR_TTT_CACHE=6
 ERR_TTT_TID_FAIL=7
@@ -540,7 +570,6 @@ ERR_CTE_PROBLEM=9
 # trop_tracker_done
 ERR_TTD_ACT_FAIL=10
 ERR_TTD_SCHG=11
-ERR_TTD_UNKNOWN_ACT=12
 
 # trop_mtl_common
 ERR_TMTLC_MV_FAIL=13
@@ -586,6 +615,9 @@ trop_errors ()
 	$ERR_PC_NO_INPUT )
 		_ 'pipe_check(): no input'
 		;;
+	$ERR_TT_UNKNOWN_OPT )
+		_ "trop_torrent(): unknown option, please check tr-remote for valid options."
+		;;
 	$ERR_TTT_CACHE )
 		_ 'trop_tracker_total(): caching error'
 		;;
@@ -603,11 +635,6 @@ trop_errors ()
 		;;
 	$ERR_TTD_SCHG )
 		_ 'trop_tracker_done(): torrent already scheduled for action'
-		;;
-	$ERR_TTD_UNKNOWN_ACT )
-		_ "trop_tracker_done(): unknown action -- actions include:\n" \
-		  " rm [hard] - Remove torrent when done. Adding \`hard' will remove files as well.\n"\
-		  " stop - Stop the torrent when done. This will halt seeding of the torrent."
 		;;
 	$ERR_TMTLC_MV_FAIL )
 		_ 'trop_mtl_common(): failed to move torrent `' "$2" "'"

README
@@ -23,8 +23,10 @@ is not guaranteed to work.
 Updating
 =========
 Starting with trop 1.4.0, you can update the core files through install.sh by supplying
-`up' or `update' as an argument. This currently does _not_ update files
-that the user might have edited themselves (tropriv.sh, trop.conf)
+`up' or `update' as an argument.
+Starting with trop 1.6.0, install.sh will also check user modifiable files for updates.
+However, trop.conf _did_ change in v1.6 but cannot be checked as the checksum is cached
+for the first time in this update. **Please update manually!**
 
 Storing tr-remote authentication information
 =============================================

trop.1
@@ -155,23 +155,23 @@ and get more information about a specific torrent from this output.
 .It Fl tt Ar tracker-alias
 Show the total amount downloaded from a tracker set as seen by the trackers (free-leech downloads are not included).
 .It Fl t Ar ID Ar option
-Pass a single option to a torrent.
-The option to supply is any valid option coupled with the tr-remote `-t' switch, however, the leading dash is removed.
+Pass an option to a torrent.
+The option to supply is any valid option coupled with the tr-remote `-t' switch, however, the leading dashes are removed.
 .It Fl td Ar ID Ar action
 Add torrent to a queue that will perform
 .Ar action
 when it completes.
+.Ar action
+is any valid option coupled with the tr-remote `-t' switch, however the leading dashes _must_ be removed.
+This is the same format as the
+.Fl t
+switch.
 The variables ADD_TRACKER_DONE and ADD_TRACKER_DONE_ACT in
 .Pa trop.conf
 are used to control the behavior of this command.
 See
 .Pa README
 for more information.
-Current actions include:
-.Dl Sy rm Op Ar hard
-.Dl Remove torrent when done. Adding `hard' will remove files as well.
-.Dl Sy stop
-.Dl Stop the torrent when done. This will halt seeding of the torrent.
 .It Fl notd
 Stop torrent from being added to the torrent-done queue if torrents are automatically being added with the ADD_TORRENT_DONE option in
 .Pa trop.conf .

trop.conf
@@ -15,8 +15,9 @@ STARTUP_CMD=
 ADD_TORRENT_DONE='no'
 
 # this is the default action to perform when a torrent finishes
-# see the man page for a list of actions
-ADD_TORRENT_DONE_ACT='rm'
+# see the man page for information about the correct formatting
+# of this option
+ADD_TORRENT_DONE_ACT='r'
 
 # enables logging automatic trop command output to a log file
 TROP_LOG='yes'

commit d577bd13d42b4451d96dd98bd92339eaf2940840

have -tt handle 3 parameters
This was a regression from commit 24f2145

trop.sh
@@ -346,6 +346,7 @@ trop_torrent ()
 
 	[ ${#2} -gt 2 ] && opt="--${2}" || opt="-${2}"
 	{ [ $thirdopt -eq 1 ]                                 && \
+	ttshift=1                                             && \
 	transmission-remote $(hpc) -n "$AUTH" -t $1 $opt "$3" || \
 	transmission-remote $(hpc) -n "$AUTH" -t $1 $opt         \
 	;} || die $ERR_TR_FAIL
@@ -911,9 +912,11 @@ while [ "$1" != '' ]; do
 		else
 			shift
 		fi
-		trop_torrent $1 $2
+		ttshift=0
+		trop_torrent "$@"
 		# over-shifting produces garbage
-		test -n "$2" ; shift $(($? ? 1 : 2))
+		test -n "$2" ; shift $(($? ? 1 : $((ttshift ? 3 : 2))))
+		unset ttshift
 		;;
 	-tdl|-tns|-tul|-t[mst]|-p)
 		[ -z "$2" ] && die $ERR_BAD_ARGS "for \`${1}'"
d4747ca9476cf85e4c0ed9cd78c3df150d3c21eb
bk <bkazemi@users.noreply.github.com>
2015-11-24 03:20:23 -0800

    escape `&' and `$' in -t
    To ensure the correct number of positional parameters are set, eval
    must be used. However, eval may interpret & and $ as shell keywords.
    To avoid this, we add a backslash to these characters. If the user
    explicity added a backslash themselves then we don't add one.
    
    Other changes
    trop.awk:
     - Check for the `Tracker gave an error' line when checking for tracker
       errors.

--- trop.sh
+++ trop.sh
@@ -907,7 +907,8 @@ while [ "$1" != '' ]; do
 			[ ! "$2" ] && die $ERR_BAD_ARGS "for \`-t'"
 			one=${1#-t}
 			echo ${one} | grep -qE '[^0-9,-]' && die $ERR_BAD_FORMAT "for \`-t'"
-			shift ; savenextopts="$@"
+			shift
+			savenextopts="$(echo "$@" | sed -r 's/[^\\]&|$/\\&\1/g')"
 			eval set -- ${one} "$savenextopts"
 			unset one savenextopts
 		else

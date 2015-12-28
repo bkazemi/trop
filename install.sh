38fd7d02e0b4b97abd115ad003c9b734d6149cde
bk <bkazemi@users.sf.net>
2015-12-28 04:35:35 -0800

    update trop.conf hash

--- install.sh
+++ install.sh
@@ -50,7 +50,7 @@ if [ -n "$1" ]; then
 		fi
 	done
 	# don't edit!
-	CUR_TROPCONF_SHA256='e71bae449b2a2d28a95d9b4ce6d6868c14f75275bfd629f94ec2bc3f1de3fb54'
+	CUR_TROPCONF_SHA256='04f877405429de2dbe7734cb82d3eec4ff4b521f0fb161477ed266cabf5c7cd9'
 	CUR_TROPRIV_SHA256='04e95cfc0391674f0591a5e7f7a2ca995a5d16afffc811ac09747d511cd00549'
 	hash sha256 2>/dev/null || err 'need sha256 to check trop.conf and tropriv.sh'
 	tropconf_sha256=`sha256 -q trop.conf`
